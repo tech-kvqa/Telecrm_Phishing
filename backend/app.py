78d79ba7c47292749e61ee7b5be646743970942b
tech-kvqa <tech@kvqaindia.com>
2025-10-27 10:28:20 +0000

    frontend code updated

--- backend/app.py
+++ backend/app.py
@@ -1822,8 +1822,8 @@ def send_email():
         # tracking_link = f"https://phishing-mail-application.onrender.com/phishing_test/{colleague.id}"
         # tracking_link = f"https://phishing-mail-frontend.vercel.app/phishing_test/{colleague.id}"
         # tracking_link = f"http://localhost:8080/phishing_test/{colleague.id}"
-        # tracking_link = f"https://phishing-application-demo-nqsy.vercel.app/phishing_test/{colleague.id}"
-        tracking_link = f"http://localhost:8080/phishing_test/{colleague.id}"
+        tracking_link = f"https://telecrm-phishing.vercel.app/phishing_test/{colleague.id}"
+        # tracking_link = f"http://localhost:8080/phishing_test/{colleague.id}"
 
         print(f"Generated tracking link for {colleague.name}: {tracking_link}")
 
@@ -2072,8 +2072,8 @@ def submit_answers(colleague_id):
         report.completion_date = datetime.now()
         db.session.commit()
 
-        study_material_link = f"http://localhost:8080/study-material/{colleague_id}"
-        # study_material_link = f"https://phishing-application-demo-nqsy.vercel.app/study-material/{colleague_id}"
+        # study_material_link = f"http://localhost:8080/study-material/{colleague_id}"
+        study_material_link = f"https://telecrm-phishing.vercel.app/study-material/{colleague_id}"
 
         if report.score >= 70:
             subject = "Congratulations on Completing the Training Program!"
@@ -2539,8 +2539,8 @@ def send_reminder(report_id):
             colleague_email = report.colleague.email
             colleague_id = report.colleague_id
 
-            study_material_link = f"http://localhost:8080/study-material/{colleague_id}"
-            # study_material_link = f"https://phishing-application-demo-nqsy.vercel.app/study-material/{colleague_id}"
+            # study_material_link = f"http://localhost:8080/study-material/{colleague_id}"
+            study_material_link = f"https://telecrm-phishing.vercel.app/study-material/{colleague_id}"
 
             msg = MIMEMultipart()
             msg['From'] = from_email
